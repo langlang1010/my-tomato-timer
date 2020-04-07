f0df133eb3521bf7722acf3fa0bfab7fbcaeac3e
langlang1010 <3225910491@qq.com>
2020-04-07 22:27:12 +0800

    '添加了倒计时相关内容'

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -16,37 +16,63 @@
         </span>
       </div>
     </div>
-    <div  style="margin-top:2%; height:200px">
-     <div id="timer" >
+    <div style="margin-top:2%; height:200px">
+      <div id="timer" style="text-align:center">
         <el-card class="box-card" style="float:left;width:48%">
           <div slot="header" class="clearfix">
             <el-button style="width:100%" @click="timeset">开始计时</el-button>
           </div>
-        <el-progress type="circle" :percentage="25" style="height:150px"></el-progress>
-        </el-card>
-      </div>
-      <div id="task" >
-         <el-card class="box-card" style="float:right; width:48%" >
-          <div slot="header" class="clearfix">
-           <el-input v-model="input" placeholder="现在要做什么" style="width:100%"></el-input>
+          <div>
+            <el-progress type="circle" style="height:150px" :percentage="time_percentage"></el-progress>
+            <p v-if="seconds === 0">{{minutes}}:{{seconds}}0</p>
+            <p v-else>{{minutes}}:{{seconds}}</p>
           </div>
-          <div  style="height:150px"> </div>
         </el-card>
       </div>
+      <div id="task">
+        <el-card class="box-card" style="float:right; width:48%">
+          <div slot="header" class="clearfix">
+            <el-input v-model="input_msg" placeholder="现在要做什么" style="width:100%"></el-input>
+          </div>
+          <div style="height:172px"></div>
+        </el-card>
       </div>
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: "Main",
-  data(){
-    return {
-      totaltime:25
-    }
-  },
-  methods:{
-    timeset(){
 
+  data() {
+    return {
+      maxtime: 10,
+      minutes: 25,
+      seconds: 0,
+      timer: "",
+      time_percentage: 100,
+      input_msg: ''
+    };
+  },
+  methods: {
+    countdown() {
+      if (this.seconds === 0) {
+        if (this.minutes === 0) {
+          return;
+        }
+        this.minutes = this.minutes - 1;
+        this.seconds = 59;
+      } else {
+        this.seconds = this.seconds - 1;
+      }
+
+      // 计算剩余时间的百分数
+      this.time_percentage = parseInt(
+        (this.minutes * 60 + this.seconds) / (25 * 0.6)
+      );
+    },
+    timeset() {
+      this.timer = setInterval(this.countdown, 1000);
     }
   }
 };
@@ -55,17 +81,13 @@ export default {
 * {
   margin: 0;
 }
-.total{
- margin-left: 10%;
+.total {
+  margin-left: 10%;
   margin-right: 10%;
   width: 80%;
 }
 .top {
- 
   height: 130px;
-  outline: none;
-  border: none;
-  border-bottom: 1px solid #ccc;
 }
 #fontstyle {
   vertical-align: middle;
@@ -79,24 +101,24 @@ export default {
   float: right;
   padding-top: 5%;
 }
- .text {
-    font-size: 14px;
-  }
+.text {
+  font-size: 14px;
+}
 
-  .item {
-    margin-bottom: 18px;
-  }
+.item {
+  margin-bottom: 18px;
+}
 
-  .clearfix:before,
-  .clearfix:after {
-    display: table;
-    content: "";
-  }
-  .clearfix:after {
-    clear: both
-  }
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both;
+}
 
-  .box-card {
-    width: 480px;
-  }
+.box-card {
+  width: 480px;
+}
 </style>
