81f473f6990322d217bbf09adb026d7f47ba020e
langlang1010 <3225910491@qq.com>
2020-04-08 22:21:21 +0800

    添加任务

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -44,15 +44,33 @@
               style="width:100%"
             ></el-input>
           </div>
-          <div class="infinite-list" style="height:172px;overflow:auto">
-            <el-checkbox
-              v-model="checked"
-              @click="finish"
-              v-for="(item, index) in list"
-              :key="index"
-              style="display:block"
-            >{{item}}</el-checkbox>
-          </div>
+          <template>
+            <div style="height:172px;overflow-y:scroll">
+              <el-row>
+                <el-col :span="12">
+                  <div class="grid-content bg-purple">
+                    <el-checkbox
+                      v-for="item in list"
+                      :key="item.id"
+                      @change="checkclick(item.id)"
+                      :checked="false"
+                      style="display:block;"
+                    >{{item.taskName}}</el-checkbox>
+                  </div>
+                </el-col>
+                <el-col :span="12">
+                  <div class="grid-content bg-purple-light">
+                    <el-checkbox
+                      v-for="item in list_finished"
+                      :key="item.id"
+                      :checked="false"
+                       style="display:block;"
+                    >{{item.taskName}}</el-checkbox>
+                  </div>
+                </el-col>
+              </el-row>
+            </div>
+          </template>
         </el-card>
       </div>
     </div>
@@ -61,22 +79,54 @@
 <script>
 export default {
   name: "Main",
-
   data() {
     return {
       maxtime: 10,
       minutes: 25,
       seconds: 0,
       timer: "",
+      chencknow: true,
       time_percentage: 100,
       input_msg: "",
       checked: false,
       running: false,
       message: "任务",
-      list: []
+      props: {
+        label: "name",
+        children: "zones",
+        isLeaf: "leaf"
+      },
+      list: [],
+      list_finished: []
     };
   },
+  mounted: function() {
+    this.loadTaskData();
+  },
   methods: {
+    loadTaskData() {
+      // 从后台拿到数据
+      var url =
+        "https://smileyan.cn/demo/user/tasks/4fb1131f57fd42529a56334e4dcacb06";
+      this.axios.get(url).then(response => {
+        let res = response.data;
+        // 把后台拿到的数据保存到this.list
+        var total_list = res.data;
+        // 判断需要删除的元素
+        var list1 = [];
+        var list2 = [];
+        // 遍历
+        for (var i = 0; i < total_list.length; i++) {
+          if (total_list[i].finished === true) {
+            list1.push(total_list[i]);
+          } else {
+            list2.push(total_list[i]);
+          }
+        }
+        this.list_finished = list1;
+        this.list = list2;
+      });
+    },
     countdown() {
       if (this.seconds === 0) {
         // 如果时间都用完了
@@ -99,6 +149,7 @@ export default {
         (this.minutes * 60 + this.seconds) / (25 * 0.6)
       );
     },
+    // 开始倒计时与暂停倒计时
     timeset() {
       // 如果正在运行，就暂停（即删除定时任务）
       if (this.running === true) {
@@ -107,15 +158,70 @@ export default {
       } else {
         // 如果还没有开始运行
         this.running = true;
-        this.timer = setInterval(this.countdown, 10);
+        this.timer = setInterval(this.countdown, 1000);
       }
     },
+    // 添加任务
     pushtask() {
-      this.list.push(this.input_msg);
+      /* this.list.push(this.input_msg);*/
+      // var that = this
+      // var postData = new URLSearchParams()
+      // postData.append('username', this.login_username)
+      // postData.append('password', this.login_password)
+      // this.axios
+      //   .post('https://smileyan.cn/demo/login', postData)
+      //   .then(function(response) {
+      //     var data = response.data
+      //     that.show_message(data, '登录成功，即将跳转...')
+      //   })
+
+      var data = { taskName: this.input_msg, finished: false };
+      this.list.push(data);
       this.input_msg = "";
     },
-    finish() {
-      this.checked = !this.checked;
+    // 完成任务
+    checkclick(taskId) {
+      // alert(taskId);
+
+      // 需要提交toekn和taskId
+      var postData = new URLSearchParams();
+      var that = this;
+      postData.append("taskId", taskId);
+      postData.append("token", "4fb1131f57fd42529a56334e4dcacb06");
+      this.axios
+        .post("https://smileyan.cn/demo/user/finish", postData)
+        .then(function(response) {
+          var data = response.data;
+          console.log(data);
+          // alert(data.msg)
+          that.loadTaskData();
+          // that.show_message(data, '修改成')
+        });
+      // alert(this.list)
+      // var data = { taskName: this.input_msg, finished: false };
+      // this.list.push(data);
+      // this.input_msg = "";
+    },
+
+    loadNode(node, resolve) {
+      if (node.level === 0) {
+        return resolve([{ name: "region" }]);
+      }
+      if (node.level > 1) return resolve([]);
+
+      setTimeout(() => {
+        const data = [
+          {
+            name: "leaf",
+            leaf: true
+          },
+          {
+            name: "zone"
+          }
+        ];
+
+        resolve(data);
+      }, 500);
     }
   }
 };
