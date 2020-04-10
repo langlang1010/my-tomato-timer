2554177da1261e54305cac92163f03b175563818
langlang1010 <3225910491@qq.com>
2020-04-10 13:55:13 +0800

    基本实现框架

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -10,44 +10,32 @@
         </div>
       </div>
       <div id="user">
-        <!-- <span style="color:#66CCFF">你好，{{username}}</span>
-        <span>
-          <img src="../assets/out.png" style="vertical-align: middle" />
-        </span>
-        -->
         <el-dropdown @command="handleCommand">
           <span class="el-dropdown-link" style="color:#66CCFF">
-            你好,{{ username }}<i class="el-icon-arrow-down el-icon--right"></i>
+            你好,{{ username }}
+            <i class="el-icon-arrow-down el-icon--right"></i>
           </span>
           <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item command="注销" icon="el-icon-switch-button"
-              >注销</el-dropdown-item
-            >
+            <el-dropdown-item command="注销" icon="el-icon-switch-button">注销</el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
       </div>
     </div>
+    <el-container>
+      <el-main>
     <div style="margin-top:2%; height:200px">
       <div id="timer" style="text-align:center">
         <el-card class="box-card" style="float:left;width:48%">
           <div slot="header" class="clearfix">
             <template v-if="running === false">
-              <el-button style="width:100%" @click="timeset"
-                >开始计时</el-button
-              >
+              <el-button style="width:100%" @click="timeset">开始计时</el-button>
             </template>
             <template v-else>
-              <el-button style="width:100%" @click="timeset"
-                >暂停计时</el-button
-              >
+              <el-button style="width:100%" @click="timeset">暂停计时</el-button>
             </template>
           </div>
           <div>
-            <el-progress
-              type="circle"
-              style="height:150px"
-              :percentage="time_percentage"
-            ></el-progress>
+            <el-progress type="circle" style="height:150px" :percentage="time_percentage"></el-progress>
             <p v-if="seconds === 0">{{ minutes }}:{{ seconds }}0</p>
             <p v-else>{{ minutes }}:{{ seconds }}</p>
           </div>
@@ -67,6 +55,7 @@
             <div style="height:172px;overflow-y:scroll">
               <el-row>
                 <el-col :span="12">
+                  <p style="color:#66CCFF">需要完成</p>
                   <div class="grid-content bg-purple">
                     <el-checkbox
                       v-for="item in list"
@@ -74,11 +63,11 @@
                       @change="checkclick(item.id)"
                       :checked="false"
                       style="display:block;"
-                      >{{ item.taskName }}</el-checkbox
-                    >
+                    >{{ item.taskName }}</el-checkbox>
                   </div>
                 </el-col>
                 <el-col :span="12">
+                  <p style="color:#66CCFF">已经完成</p>
                   <div class="grid-content bg-purple-light">
                     <el-checkbox
                       v-for="item in list_finished"
@@ -86,8 +75,7 @@
                       @change="checkfinish(item.id)"
                       :checked="false"
                       style="display:block;"
-                      >{{ item.taskName }}</el-checkbox
-                    >
+                    >{{ item.taskName }}</el-checkbox>
                   </div>
                 </el-col>
               </el-row>
@@ -96,185 +84,193 @@
         </el-card>
       </div>
     </div>
+    </el-main>
+       <el-footer>
+         <p>累计完成</p>
+         <p style="color:#66CCFF;font-size:2rem">{{list_finished.length}}</p>
+         <p>个任务</p>
+       </el-footer>
+    </el-container>
   </div>
+  
 </template>
 <script>
 export default {
-  name: 'Main',
+  name: "Main",
   data() {
     return {
       maxtime: 10,
       minutes: 25,
       seconds: 0,
-      timer: '',
+      timer: "",
       chencknow: true,
       time_percentage: 100,
-      input_msg: '',
+      input_msg: "",
       checked: false,
       running: false,
-      message: '任务',
+      message: "任务",
       props: {
-        label: 'name',
-        children: 'zones',
-        isLeaf: 'leaf',
+        label: "name",
+        children: "zones",
+        isLeaf: "leaf"
       },
       list: [],
       list_finished: [],
-      username: '',
-    }
+      username: ""
+    };
   },
   mounted: function() {
-    this.loadTaskData()
+    this.loadTaskData();
   },
   methods: {
     loadTaskData() {
       // 获得授权码
-      var that = this
-      var token = window.localStorage['token']
+      var that = this;
+      var token = window.localStorage["token"];
       if (token === null) {
-        alert('fffff')
+        alert("fffff");
       }
-      var url = 'https://smileyan.cn/demo/user/tasks/' + token
+      var url = "https://smileyan.cn/demo/user/tasks/" + token;
       // 拿到用户名
-      this.username = window.localStorage['username']
+      this.username = window.localStorage["username"];
       // 把后台拿到的数据保存到this.list
-      this.axios.get(url).then((response) => {
-        let res = response.data
+      this.axios.get(url).then(response => {
+        let res = response.data;
         if (res.code === 200) {
-          var total_list = res.data
+          var total_list = res.data;
           // 判断需要删除的元素
-          var list1 = []
-          var list2 = []
+          var list1 = [];
+          var list2 = [];
           // 遍历
           for (var i = 0; i < total_list.length; i++) {
             if (total_list[i].finished === true) {
-              list1.push(total_list[i])
+              list1.push(total_list[i]);
             } else {
-              list2.push(total_list[i])
+              list2.push(total_list[i]);
             }
           }
           // 把最近的放在前面
-          this.list_finished = list1.reverse()
-          this.list = list2.reverse()
+          this.list_finished = list1.reverse();
+          this.list = list2.reverse();
         } else {
           this.$message({
-            message: res.msg + '，即将前去登录...',
-            type: 'error',
+            message: res.msg + "，即将前去登录...",
+            type: "error",
             duration: 1000,
             onClose: function() {
-              that.$router.push({ path: '/' })
-            },
-          })
+              that.$router.push({ path: "/" });
+            }
+          });
         }
-      })
+      });
     },
     countdown() {
       if (this.seconds === 0) {
         // 如果时间都用完了
         if (this.minutes === 0) {
-          this.running = false
-          this.minutes = 25
+          this.running = false;
+          this.minutes = 25;
           // 需要clear一下时间
-          window.clearInterval(this.timer)
-          this.time_percentage = 100
-          return
+          window.clearInterval(this.timer);
+          this.time_percentage = 100;
+          return;
         }
-        this.minutes = this.minutes - 1
-        this.seconds = 59
+        this.minutes = this.minutes - 1;
+        this.seconds = 59;
       } else {
-        this.seconds = this.seconds - 1
+        this.seconds = this.seconds - 1;
       }
 
       // 计算剩余时间的百分数
       this.time_percentage = parseInt(
         (this.minutes * 60 + this.seconds) / (25 * 0.6)
-      )
+      );
     },
     // 注销登录
     handleCommand(command) {
       // 1. 清空localstorage
-      window.localStorage['token'] = null
+      window.localStorage["token"] = null;
       // 2. 跳转登录界面
-      var that = this
+      var that = this;
       this.$message({
-        message: command + '注销成功，前往登录界面',
-        type: 'success',
+        message: command + "注销成功，前往登录界面",
+        type: "success",
         duration: 3000,
         onClose: function() {
-          that.$router.push({ path: '/' })
-        },
-      })
+          that.$router.push({ path: "/" });
+        }
+      });
     },
     // 开始倒计时与暂停倒计时
     timeset() {
       // 如果正在运行，就暂停（即删除定时任务）
       if (this.running === true) {
-        window.clearInterval(this.timer)
-        this.running = false
+        window.clearInterval(this.timer);
+        this.running = false;
       } else {
         // 如果还没有开始运行
-        this.running = true
-        this.timer = setInterval(this.countdown, 1000)
+        this.running = true;
+        this.timer = setInterval(this.countdown, 1000);
       }
     },
     // 添加任务
     pushtask() {
-      var taskName = this.input_msg
-      var that = this
-      var token = window.localStorage['token']
-      var postData = new URLSearchParams()
-      if (taskName.trim() === '') {
-        return
+      var taskName = this.input_msg;
+      var that = this;
+      var token = window.localStorage["token"];
+      var postData = new URLSearchParams();
+      if (taskName.trim() === "") {
+        return;
       } else {
-        postData.append('taskName', taskName)
-        postData.append('token', token)
+        postData.append("taskName", taskName);
+        postData.append("token", token);
         this.axios
-          .post('https://smileyan.cn/demo/user/add', postData)
+          .post("https://smileyan.cn/demo/user/add", postData)
           .then(function(response) {
-            let res = response.data
-            var task = res.data
+            let res = response.data;
+            var task = res.data;
             // 第一次倒序
-            that.list = that.list.reverse()
+            that.list = that.list.reverse();
             // 添加在最后
-            that.list.push(task)
+            that.list.push(task);
             // 再次倒过来
-            that.list = that.list.reverse()
-            that.show_message(res.msg, '添加成功')
-          })
+            that.list = that.list.reverse();
+            that.show_message(res.msg, "添加成功");
+          });
       }
-      this.input_msg = ''
+      this.input_msg = "";
     },
     // 完成任务
     checkclick(taskId) {
-      var token = '' + window.localStorage['token']
-      var postData = new URLSearchParams()
-      var that = this
-      postData.append('taskId', taskId)
-      postData.append('token', token)
+      var token = "" + window.localStorage["token"];
+      var postData = new URLSearchParams();
+      var that = this;
+      postData.append("taskId", taskId);
+      postData.append("token", token);
       this.axios
-        .post('https://smileyan.cn/demo/user/finish', postData)
+        .post("https://smileyan.cn/demo/user/finish", postData)
         .then(function(response) {
-          var data = response.data
-          console.log(data)
-          that.loadTaskData()
-        })
+          var data = response.data;
+          console.log(data);
+          that.loadTaskData();
+        });
     },
     checkfinish(taskId) {
-      var token = '' + window.localStorage['token']
-      var postData = new URLSearchParams()
-      var that = this
-      postData.append('taskId', taskId)
-      postData.append('token', token)
+      var token = "" + window.localStorage["token"];
+      var postData = new URLSearchParams();
+      var that = this;
+      postData.append("taskId", taskId);
+      postData.append("token", token);
       this.axios
-        .post('https://smileyan.cn/demo/user/restart', postData)
+        .post("https://smileyan.cn/demo/user/restart", postData)
         .then(function(response) {
-          var data = response.data
-          console.log(data)
-          that.loadTaskData()
-        })
-    },
-  },
-}
+          var data = response.data;
+          console.log(data);
+          that.loadTaskData();
+        });
+    }
+  }
+};
 </script>
 <style scoped>
 * {
@@ -311,7 +307,7 @@ export default {
 .clearfix:before,
 .clearfix:after {
   display: table;
-  content: '';
+  content: "";
 }
 .clearfix:after {
   clear: both;
